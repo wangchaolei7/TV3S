dd4450471d750c190294d0ced3a15c4fe9fa906c
wangchaolei7 <ddwang2121@gmail.com>
2025-12-31 05:50:01 +0800

    [kitti360多卡训练] ./tools/dist_train.sh   local_configs/tv3s/B5/tv3s_realshift_w20_s10.b5.480x480.kitti36015.160k.py 6   --work-dir /data1/wangcl/project/TV3S/kitti360   --no-validate

--- utils/datasets/__init__.py
+++ utils/datasets/__init__.py
@@ -3,5 +3,6 @@ from .custom import *
 from .dataset_pipelines import *
 from .transforms import *
 from .apollo import *
+from .kitti360 import *
 from .cityscapes_corruptions import *
 from .vspw import *
