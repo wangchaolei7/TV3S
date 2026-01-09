7c3eb48bc69498655c97a22337088cb328bbadb2
wangchaolei7 <ddwang2121@gmail.com>
2026-01-09 15:30:49 +0800

    [Kitti360、Camvid源域推理可视化脚本] CUDA_VISIBLE_DEVICES=0,1,3,4,5 NPROC=5 bash tools/run_eval_all_datasets_kitti360.sh CUDA_VISIBLE_DEVICES=0,1,3,4,5 NPROC=5 bash tools/run_eval_all_datasets_camvid.sh

--- tools/run_eval_all_datasets_camvid.sh
+++ tools/run_eval_all_datasets_camvid.sh
@@ -6,8 +6,8 @@ REPO_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
 cd "${REPO_ROOT}"
 
 NPROC="${NPROC:-6}"
-CKPT="${CKPT:-/data1/wangcl/project/TV3S/apollo/iter_14000.pth}"
-OUT_ROOT="${OUT_ROOT:-/data1/wangcl/project/TV3S/apollo}"
+CKPT="${CKPT:-/data1/wangcl/project/TV3S/camvid/iter_62000.pth}"
+OUT_ROOT="${OUT_ROOT:-/data1/wangcl/project/TV3S/camvid}"
 
 run_eval() {
   local name="$1"
@@ -24,7 +24,7 @@ run_eval() {
 
 run_eval "apollo" "local_configs/tv3s/B5/tv3s_realshift_w20_s10.b5.480x480.apollo15.160k.py"
 run_eval "camvid" "local_configs/tv3s/B5/tv3s_realshift_w20_s10.b5.480x480.camvid15.160k.py"
-# run_eval "kitti360" "local_configs/tv3s/B5/tv3s_realshift_w20_s10.b5.480x480.kitti36015.160k.py"
+run_eval "kitti360" "local_configs/tv3s/B5/tv3s_realshift_w20_s10.b5.480x480.kitti36015.160k.py"
 run_eval "cityscapes_origin" "local_configs/tv3s/B5/cityscapes_corruptions/tv3s_b5_city_origin.py"
 run_eval "cityscapes_fog" "local_configs/tv3s/B5/cityscapes_corruptions/tv3s_b5_city_fog.py"
 run_eval "cityscapes_frost" "local_configs/tv3s/B5/cityscapes_corruptions/tv3s_b5_city_frost.py"
